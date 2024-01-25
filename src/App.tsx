7a82c6734b2ab8d128bedc83dacea42c47e5f92f
Nabin Katwal <91274113+nabinkatwal7@users.noreply.github.com>
2024-01-25 10:52:45 +0545

    sample 1

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,71 @@ import "./App.css";
 
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl text-center text-red-700">
-        Welcome to Vite with TailwindCSS and React
-      </h1>
+    <div className="c-body">
+      <div className="page">
+        <div id="content" className="main">
+          <section className="section s-hero s-hero-versions">
+            <div className="section-radius"></div>
+            <div className="container-max">container-max</div>
+          </section>
+          <div className="section s-heading-first background-blurple z-9">
+            <div className="container-max">
+              <div className="flex-grid">
+                <div className="col-wrapper">
+                  <div className="col col-12-d">
+                    <div className="heading-lines hide-mobile-wide">
+                      <div className="heading-line hl-first"></div>
+                      <div className="heading-line"></div>
+                    </div>
+
+                    <div className="flex-grid">Template</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <section className="section s-radius sr-large background-blurple z-9">
+            <div className="container-max">
+              <div className="flex-grid">
+                <p>
+                  Template, template, template, template, template, template,
+                  template, template, template, template, template, template,
+                  template, template, template, template, template, template,
+                  template, template, template, template, template, template,
+                  template, template, template, template, template, template,
+                  template, template, template, template, template, template,
+                  template
+                </p>
+              </div>
+            </div>
+          </section>
+
+          <div className="section s-heading background-white z-7">
+            <div className="section-head-bg-extension background-white"></div>
+            <div className="container-max">
+              <h2 className="heading-xl text-center">Template text-center</h2>
+            </div>
+          </div>
+
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+        </div>
+      </div>
     </div>
   );
 }
