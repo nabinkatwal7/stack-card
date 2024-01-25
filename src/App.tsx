8de8c26b36850621fb744d19e57da64aafa6f681
Nabin Katwal <91274113+nabinkatwal7@users.noreply.github.com>
2024-01-25 11:09:02 +0545

    sample 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,87 @@
-import { useState, useLayoutEffect } from "react";
+import React, { useState, useLayoutEffect } from "react";
 import gsap from "gsap";
 import ScrollTrigger from "gsap/dist/ScrollTrigger";
 import "./App.css";
 
 function App() {
-  const cards = document.querySelectorAll(".card");
-  const header = document.querySelector(".header");
-  const animation = gsap.timeline();
-  let cardHeight;
 
   useLayoutEffect(() => {
     gsap.registerPlugin(ScrollTrigger);
 
-    function initCards() {
-      animation.clear();
-      cardHeight = cards[0].offsetHeight;
-      console.log("initCards()", cardHeight);
-      cards.forEach((card, index) => {
-        if (index > 0) {
-          //increment y value of each card by cardHeight
-          gsap.set(card, { y: index * cardHeight });
-          //animate each card back to 0 (for stacking)
-          animation.to(card, { y: 0, duration: index * 0.5, ease: "none" }, 0);
-        }
-      });
-    }
+    gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
 
-    initCards();
-
-    ScrollTrigger.create({
-      trigger: ".wrapper",
-      start: "top top",
-      pin: true,
-      end: () => `+=${cards.length * cardHeight + header.offsetHeight}`,
-      scrub: true,
-      animation: animation,
-      markers: true,
-      invalidateOnRefresh: true,
+    let timeln = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".cards",
+        pin: true,
+        pinSpacing: true,
+        start: "left-=120px left",
+        end: "+=2000",
+        scrub: 1,
+      },
     });
 
-    ScrollTrigger.addEventListener("refreshInit", initCards);
-  }, []);
+    timeln.addLabel("card1");
+    timeln.to(".card-1", {
+      xPercent: 0,
+      opacity: 1,
+    });
+
+    timeln.from(".card-2", {
+      xPercent: 75,
+      opacity: 0,
+    });
+    timeln.addLabel("card2");
+
+    timeln.to(
+      ".card-1",
+      {
+        scale: 0.95,
+        xPercent: -0.5,
+        opacity: 0.5,
+      },
+      "-=0.3"
+    );
+
+    timeln.to(".card-2", {
+      xPercent: 0,
+      opacity: 1,
+    });
+
+    timeln.from(".card-3", {
+      xPercent: 75,
+      opacity: 0,
+    });
+    timeln.addLabel("card3");
+
+    timeln.to(
+      ".card-2",
+      {
+        scale: 0.98,
+        xPercent: -0.4,
+        opacity: 0.6,
+      },
+      "-=0.3"
+    );
+
+    timeln.to(".card-3", {
+      xPercent: 0,
+      opacity: 1,
+    });
+
+    timeln.to(".card-3", {});
+  },[])
 
   return (
-    <div>
-      <div className="spacer h-[50vh]">Scroll Down</div>
-      <div className="wrapper">
-        <div className="header">
-          GSAP SCROLLTRIGGER
-          <br /> <span>FREE DEMO</span>
-        </div>
-        <div className="cards">
-          <div className="card">stacking</div>
-          <div className="card">cards</div>
-          <div className="card">for</div>
-          <div className="card">you</div>
-          <div className="card">to</div>
-          <div className="card">explore</div>
-          <div className="card">and</div>
-          <div className="card">use</div>
-        </div>
+    <div className="cards">
+      <div className="card card-1">
+        <h2>01</h2>
       </div>
-      <div className="spacer h-[100vh] text-center">
-        SEE VIDEO DESCRIPTION FOR DEMO LINK
+      <div className="card card-2">
+        <h2>02</h2>
+      </div>
+      <div className="card card-3">
+        <h2>03</h2>
       </div>
     </div>
   );
