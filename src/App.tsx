174709d33ec8a14f660c48c77cfcd43edbdd1f8e
Nabin Katwal <91274113+nabinkatwal7@users.noreply.github.com>
2024-01-25 11:00:40 +0545

    sample 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,71 +1,68 @@
+import { useState, useLayoutEffect } from "react";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/dist/ScrollTrigger";
 import "./App.css";
 
 function App() {
+  const cards = document.querySelectorAll(".card");
+  const header = document.querySelector(".header");
+  const animation = gsap.timeline();
+  let cardHeight;
+
+  useLayoutEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    function initCards() {
+      animation.clear();
+      cardHeight = cards[0].offsetHeight;
+      console.log("initCards()", cardHeight);
+      cards.forEach((card, index) => {
+        if (index > 0) {
+          //increment y value of each card by cardHeight
+          gsap.set(card, { y: index * cardHeight });
+          //animate each card back to 0 (for stacking)
+          animation.to(card, { y: 0, duration: index * 0.5, ease: "none" }, 0);
+        }
+      });
+    }
+
+    initCards();
+
+    ScrollTrigger.create({
+      trigger: ".wrapper",
+      start: "top top",
+      pin: true,
+      end: () => `+=${cards.length * cardHeight + header.offsetHeight}`,
+      scrub: true,
+      animation: animation,
+      markers: true,
+      invalidateOnRefresh: true,
+    });
+
+    ScrollTrigger.addEventListener("refreshInit", initCards);
+  }, []);
+
   return (
-    <div className="c-body">
-      <div className="page">
-        <div id="content" className="main">
-          <section className="section s-hero s-hero-versions">
-            <div className="section-radius"></div>
-            <div className="container-max">container-max</div>
-          </section>
-          <div className="section s-heading-first background-blurple z-9">
-            <div className="container-max">
-              <div className="flex-grid">
-                <div className="col-wrapper">
-                  <div className="col col-12-d">
-                    <div className="heading-lines hide-mobile-wide">
-                      <div className="heading-line hl-first"></div>
-                      <div className="heading-line"></div>
-                    </div>
-
-                    <div className="flex-grid">Template</div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <section className="section s-radius sr-large background-blurple z-9">
-            <div className="container-max">
-              <div className="flex-grid">
-                <p>
-                  Template, template, template, template, template, template,
-                  template, template, template, template, template, template,
-                  template, template, template, template, template, template,
-                  template, template, template, template, template, template,
-                  template, template, template, template, template, template,
-                  template, template, template, template, template, template,
-                  template
-                </p>
-              </div>
-            </div>
-          </section>
-
-          <div className="section s-heading background-white z-7">
-            <div className="section-head-bg-extension background-white"></div>
-            <div className="container-max">
-              <h2 className="heading-xl text-center">Template text-center</h2>
-            </div>
-          </div>
-
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
+    <div>
+      <div className="spacer h-[50vh]">Scroll Down</div>
+      <div className="wrapper">
+        <div className="header">
+          GSAP SCROLLTRIGGER
+          <br /> <span>FREE DEMO</span>
         </div>
+        <div className="cards">
+          <div className="card">stacking</div>
+          <div className="card">cards</div>
+          <div className="card">for</div>
+          <div className="card">you</div>
+          <div className="card">to</div>
+          <div className="card">explore</div>
+          <div className="card">and</div>
+          <div className="card">use</div>
+        </div>
+      </div>
+      <div className="spacer h-[100vh] text-center">
+        SEE VIDEO DESCRIPTION FOR DEMO LINK
       </div>
     </div>
   );
